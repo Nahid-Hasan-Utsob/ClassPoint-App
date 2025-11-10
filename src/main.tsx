33c2df942e9d2b690f552f3f42b6ab550e93be07
Nahid Hasan Utsob <mdnahidhassan555@gmail.com>
2025-11-10 23:04:38 +0600

    build(deps): update package-lock.json, package.json and main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { RouterProvider } from "react-router";
+import { RouterProvider } from "react-router-dom";
 import router from './Layout/Router'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router}>
-      <App />
     </RouterProvider>
   </StrictMode>
 );
