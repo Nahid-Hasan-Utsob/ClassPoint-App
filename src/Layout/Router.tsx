2e50a86d925937bd864e8bbd0d5e4fbbe0139f8f
Nahid Hasan Utsob <mdnahidhassan555@gmail.com>
2025-11-10 23:05:04 +0600

    update Router.tsx, Home_Main.tsx and main.tsx

--- src/Layout/Router.tsx
+++ src/Layout/Router.tsx
@@ -1,14 +1,11 @@
 import { createBrowserRouter } from "react-router";
 import Main_Layout from "./Main_Layout";
 
-
-const router = createBrowserRouter (
-      [
-            {
-                  path: '/',
-                  element: <Main_Layout></Main_Layout>
-            }
-      ]
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main_Layout></Main_Layout>,
+  },
+]);
 
 export default router;
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import router from './Layout/Router'
+import router from "./Layout/Router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}>
-    </RouterProvider>
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>
 );
