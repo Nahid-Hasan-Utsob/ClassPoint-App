004aed82825f3155195871a4e5a7e7a0c774adab
Nahid Hasan Utsob <mdnahidhassan555@gmail.com>
2025-11-10 23:00:24 +0600

    update 2 files and delete 1 file

--- src/Layout/Main_Layout.tsx
+++ src/Layout/Main_Layout.tsx
@@ -2,7 +2,7 @@
 export default function Main_Layout() {
   return (
     <div>
-      
+      <h1>done</h1>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { RouterProvider } from "react-router";
+import router from './Layout/Router'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
