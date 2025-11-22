b34606c35cf6ee7e44830acb687d00f097435bae
Nahid Hasan Utsob <mdnahidhassan555@gmail.com>
2025-11-22 10:51:38 +0600

    update Custom_Style.css and Header.tsx

--- src/Pages/Header/Header.tsx
+++ src/Pages/Header/Header.tsx
@@ -1,13 +1,25 @@
 import { Link } from 'react-router'
 import HeaderLogo from '../../assets/header-bg.png'
-
+import "../../Custom_Style.css"
 export default function Header() {
 
   const MenuLinks = <>
   
-  <ul className='flex items-center justify-evenly'>
+  <ul className='flex justify-evenly items-center h-full'>
    <Link to={'/'}>
-    <li className='text-[22px] s_text_color'>Home</li></Link>
+    <li className='text-[22px] s-text-color'>Home</li></Link>
+   <Link to={'/'}>
+    <li className='text-[22px] s-text-color'>Courses</li></Link>
+   <Link to={'/'}>
+    <li className='text-[22px] s-text-color'>Careers</li></Link>
+   <Link to={'/'}>
+    <li className='text-[22px] s-text-color'>Blog</li></Link>
+   <Link to={'/'}>
+    <li className='text-[22px] s-text-color'>About Us</li></Link>
+   <Link to={'/'}>
+    <li className='text-[22px] s-text-color'>Login</li></Link>
+   <Link to={'/'}>
+    <li className='text-[22px] s-text-color'>Sign Up</li></Link>
   </ul>
   
   
@@ -16,7 +28,7 @@ export default function Header() {
 
 
   return (
-    <section>
+    <section className='pop-regular'>
       <div className="main-bg-color h-[100px] flex items-center justify-between">
         <div>
           <img src={HeaderLogo} alt="" />
