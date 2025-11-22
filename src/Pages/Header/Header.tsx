296c29875e240f5fbf7f3d4563b6b46ae365f638
Nahid Hasan Utsob <mdnahidhassan555@gmail.com>
2025-11-22 10:43:13 +0600

    update index.html, Main_Layout.tsx and Header.tsx

--- src/Pages/Header/Header.tsx
+++ src/Pages/Header/Header.tsx
@@ -1,9 +1,29 @@
+import { Link } from 'react-router'
+import HeaderLogo from '../../assets/header-bg.png'
 
 export default function Header() {
+
+  const MenuLinks = <>
+  
+  <ul className='flex items-center justify-evenly'>
+   <Link to={'/'}>
+    <li className='text-[22px] text-white'>Home</li></Link>
+  </ul>
+  
+  
+  </>
+
+
+
   return (
     <section>
-      <div className="bg-[#65DAFF] h-[100px]">
-
+      <div className="bg-[#65DAFF] h-[100px] flex items-center justify-between">
+        <div>
+          <img src={HeaderLogo} alt="" />
+        </div>
+        <div>
+          {MenuLinks}
+        </div>
       </div>
     </section>
   )
