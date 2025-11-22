0cdd181f3c9c064d9009eeff8142ba63f5b6cf57
Nahid Hasan Utsob <mdnahidhassan555@gmail.com>
2025-11-22 11:02:14 +0600

    update Custom_Style.css and Header.tsx

--- src/Pages/Header/Header.tsx
+++ src/Pages/Header/Header.tsx
@@ -1,42 +1,49 @@
-import { Link } from 'react-router'
-import HeaderLogo from '../../assets/header-bg.png'
-import "../../Custom_Style.css"
+import { Link } from "react-router";
+import HeaderLogo from "../../assets/header-bg.png";
+import "../../Custom_Style.css";
 export default function Header() {
-
-  const MenuLinks = <>
-  
-  <ul className='flex justify-evenly items-center h-full'>
-   <Link to={'/'}>
-    <li className='text-[22px] s-text-color'>Home</li></Link>
-   <Link to={'/'}>
-    <li className='text-[22px] s-text-color'>Courses</li></Link>
-   <Link to={'/'}>
-    <li className='text-[22px] s-text-color'>Careers</li></Link>
-   <Link to={'/'}>
-    <li className='text-[22px] s-text-color'>Blog</li></Link>
-   <Link to={'/'}>
-    <li className='text-[22px] s-text-color'>About Us</li></Link>
-   <Link to={'/'}>
-    <li className='text-[22px] s-text-color'>Login</li></Link>
-   <Link to={'/'}>
-    <li className='text-[22px] s-text-color'>Sign Up</li></Link>
-  </ul>
-  
-  
-  </>
-
-
+  const MenuLinks = (
+    <>
+      <ul className="flex justify-evenly items-center h-full gap-10">
+        <Link to={"/"}>
+          <li className="text-[22px] s-text-color">Home</li>
+        </Link>
+        <Link to={"/"}>
+          <li className="text-[22px] s-text-color">Courses</li>
+        </Link>
+        <Link to={"/"}>
+          <li className="text-[22px] s-text-color">Careers</li>
+        </Link>
+        <Link to={"/"}>
+          <li className="text-[22px] s-text-color">Blog</li>
+        </Link>
+        <Link to={"/"}>
+          <li className="text-[22px] s-text-color">About Us</li>
+        </Link>
+        <div className="flex gap-8">
+          <Link to={"/"}>
+            <li className="text-[22px] main-text-color py-2 px-[50px] bg-white rounded-4xl">
+              Login
+            </li>
+          </Link>
+          <Link to={"/"}>
+            <li className="text-[22px] s-text-color py-2 px-[35px] bg-white/40 rounded-4xl">
+              Sign Up
+            </li>
+          </Link>
+        </div>
+      </ul>
+    </>
+  );
 
   return (
-    <section className='pop-regular'>
-      <div className="main-bg-color h-[100px] flex items-center justify-between">
+    <section className="pop-regular main-bg-color">
+      <div className=" h-[100px] flex items-center justify-between max-w-[1400px] mx-auto">
         <div>
-          <img src={HeaderLogo} alt="" />
+          <img className="w-[60px]" src={HeaderLogo} alt="" />
         </div>
-        <div>
-          {MenuLinks}
-        </div>
+        <div>{MenuLinks}</div>
       </div>
     </section>
-  )
+  );
 }
